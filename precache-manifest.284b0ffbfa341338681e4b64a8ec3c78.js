382e630fe88a6ef9153138ef3a8f59fae44e6275
Crystal Danger <cdneal091@gmail.com>
2019-12-01 21:50:00 -0700

    Updates

---
+++ precache-manifest.284b0ffbfa341338681e4b64a8ec3c78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c27599eb5fd3dd91fdf7582fa13cbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23b5256c717ac98ca88",
+    "url": "/static/css/main.104fae0f.chunk.css"
+  },
+  {
+    "revision": "eba161befbabaf3459e6",
+    "url": "/static/js/2.7bb9884e.chunk.js"
+  },
+  {
+    "revision": "e23b5256c717ac98ca88",
+    "url": "/static/js/main.3ae9b946.chunk.js"
+  },
+  {
+    "revision": "b2782662d6389a80dcdb",
+    "url": "/static/js/runtime-main.4f42e2eb.js"
+  }
+]);
