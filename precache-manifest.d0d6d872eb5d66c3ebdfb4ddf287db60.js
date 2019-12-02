dcf96ba61b77fb1c7b3161166d4f1e7566f57273
Crystal Danger <cdneal091@gmail.com>
2019-12-01 22:59:10 -0700

    asdfa

---
+++ precache-manifest.d0d6d872eb5d66c3ebdfb4ddf287db60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978fd5b746d279f1de83d46d694559c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2dfe981279ece47bf10",
+    "url": "/static/css/main.104fae0f.chunk.css"
+  },
+  {
+    "revision": "eba161befbabaf3459e6",
+    "url": "/static/js/2.7bb9884e.chunk.js"
+  },
+  {
+    "revision": "d2dfe981279ece47bf10",
+    "url": "/static/js/main.c436f676.chunk.js"
+  },
+  {
+    "revision": "b2782662d6389a80dcdb",
+    "url": "/static/js/runtime-main.4f42e2eb.js"
+  }
+]);
