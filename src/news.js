a5c7700b23267c833a4a5b5398263262aef0a4fa
Crystal Danger <cdneal091@gmail.com>
2020-02-24 12:26:46 -0500

    add arrival

--- src/news.js
+++ src/news.js
@@ -1,5 +1,10 @@
 export default [
   {
+    date: 'Jan. 7, 2020',
+    title: 'The Triangle',
+    text: "Finally made it to the Raleigh-Durham area, what a drive. Now to find a job. If you're reading this, hire me!"
+  },
+  {
     date: 'Nov. 25, 2019',
     title: 'Personal Site Re-Launch',
     text: "New and improved website! Now a React app and has content in the 'About' section. I have also done away with the 'Blog' section in favor of this 'News' section. I also added a Game!"
