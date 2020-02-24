620213ee794cee5200caf8aa16e9d84cafcb04b2
Crystal Danger <cdneal091@gmail.com>
2020-02-24 12:22:44 -0500

    added new projects and video links

--- src/projects.js
+++ src/projects.js
@@ -1,21 +1,37 @@
 export default [
   {
-    name: 'Personal Website',
-    description: 'The very site you are looking at! Recently renovated as a React app.',
-    live_link: 'http://crystalneal.com',
-    github_link: 'https://github.com/DangerN/DangerN.github.io'
+    name: 'Cloud Chan',
+    description: 'A social media site built on micro-services designed for cloud deployment.',
+    live_link: '',
+    demo_link: 'https://youtu.be/iIMm21s1D6s',
+    github_link: 'https://github.com/DangerN/cc-roster/blob/master/README.md'
+  },
+  {
+    name: 'Phonetic Alphabet Generator',
+    description: 'Generates a phonetic alphabet based on a given word.',
+    live_link: 'https://crystalneal.com/phonetic-alphabet-generator/',
+    demo_link: 'https://youtu.be/EG7amDsmo-8',
+    github_link: 'https://github.com/DangerN/phonetic-alphabet-generator'
   },
   {
     name: 'Bezier Curver',
     description: 'This app is designed to help visualize different values for the cubic bezier animation timing function as it pertains to CSS styling.',
     live_link: 'http://crystalneal.com/bezier-curver/',
+    demo_link: 'https://youtu.be/yEBS2AT4j3Y',
     github_link: 'https://github.com/DangerN/bezier-curver'
   },
   {
     name: 'AR Game',
     description: 'An augmented reality game intended to be played with paper markers and a smart phone. Built with Rails and AR.js',
     live_link: 'https://crystalneal.com/AR-game/',
-    github_link: 'https://github.com/DangerN/AR-game'
+    demo_link: 'https://youtu.be/EG7amDsmo-8',
+    github_link: 'https://youtu.be/rGR9RfoXKF8'
+  },
+  {
+    name: 'Personal Website',
+    description: 'The very site you are looking at! Recently renovated as a React app.',
+    live_link: 'http://crystalneal.com',
+    github_link: 'https://github.com/DangerN/DangerN.github.io'
   },
   {
     name: 'Fitness Tracker v2',
